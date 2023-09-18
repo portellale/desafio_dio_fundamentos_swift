2cf8d8147a0ae3206cbc97528cd9f703628e902b
portellale <portella_le@hotmail.com>
2023-09-18 16:18:49 -0300

    Atualizar arquivo do playground

--- desafio.swift.xcode.dio.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
+++ desafio.swift.xcode.dio.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
@@ -1,1 +1,29 @@
 
+let firstName = "Steve"
+var secondName: String? = "Jobs"
+
+// Mostra o nome com forced unwrapping
+print("\(firstName) \(secondName!)")
+
+// Mostra Wozniak usando optional binding com valor nil
+secondName = nil
+if let lastName = secondName {
+    print("\(firstName) \(lastName)")
+} else {
+    print("\(firstName) Wozniak")
+}
+
+// Mostra Jobs usando optional binding
+secondName = "Jobs"
+if let lastName = secondName {
+    print("\(firstName) \(lastName)")
+} else {
+    print("\(firstName) Wozniak")
+}
+
+
+
+
+
+
+
